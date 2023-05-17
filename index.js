b2eb62c6957b903099392085730062181aceeb31
Rmaki27 <engineermaki27@gmail.com>
2023-05-16 20:54:14 -0700

    Render new dog when liked or disliked

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import { dogs } from "./data.js";
 import Dog from "./Dog.js";
 
-const dog1 = new Dog(dogs[0]);
-
-console.log(dog1.getDogHtml());
-
-function render() {
-  document.getElementById("profile-display").innerHTML = dog1.getDogHtml();
+function getNewDog() {
+  const newDog = new Dog(dogs.shift());
+  render(newDog);
+  return newDog;
 }
 
-render();
-
 document.body.addEventListener("click", (e) => {
-  if (e.target.dataset.liked && dog1.hasBeenLiked === false) {
+  if (e.target.dataset.liked && dog.hasBeenLiked === false) {
     console.log("like button clicked");
-    // console.log(e.target.dataset.liked);
-    dog1.setMatchStatus(e.target.dataset.liked);
+    dog.setMatchStatus(e.target.dataset.liked);
+    dog = getNewDog();
   } else if (e.target.dataset.disliked) {
     console.log("dislike button clicked");
-    dog1.setMatchStatus(e.target.dataset.disliked);
+    dog.setMatchStatus(e.target.dataset.disliked);
+    dog = getNewDog();
   }
 });
+
+function render(dog) {
+  document.getElementById("profile-display").innerHTML = dog.getDogHtml();
+}
+
+// Render initial dog
+let dog = getNewDog();
